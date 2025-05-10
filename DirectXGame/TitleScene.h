4c0f50bd17fa38e40a8867a79f6740d60f8c5cc6
ohara <ohara@g.neec.ac.jp>
2025-05-10 12:12:28 +0900

    05_02.タイトル応用

--- DirectXGame/TitleScene.h
+++ DirectXGame/TitleScene.h
@@ -4,6 +4,9 @@
 class TitleScene
 {
 public:
+	// デストラクタ
+	~TitleScene();
+
 	// 初期化
 	void Initialize();
 
@@ -16,10 +19,16 @@ public:
 private:
 	// テクスチャハンドル
 	uint32_t textureHandle_ = 0;
+	uint32_t textureHandleFont_ = 0;
+	uint32_t textureHandleHit_ = 0;
+
 	// スプライト
 	KamataEngine::Sprite* sprite_ = nullptr;
+	KamataEngine::Sprite* spriteFont_ = nullptr;
+	KamataEngine::Sprite* spriteHit_ = nullptr;
 
-
+	int timer = 0;
+	int y = -100;
 
 };
 
